e3278475e6073f5190af62911a6520c54188df91
Alexey Frilund <109166904+FrilundAleksei@users.noreply.github.com>
2022-10-20 22:07:24 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -54,7 +54,7 @@ def main():
     # Create the Updater and pass it your bot's token.
     # Make sure to set use_context=True to use the new context based callbacks
     # Post version 12 this will no longer be necessary
-    updater = Updater("TOKEN", use_context=True)
+    updater = Updater("TOKEN", use_context=True)                           # заменить токен на свой
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
